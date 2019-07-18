6ff1b75b8b56f88be15d10f9cab90b4e0a09388b
Rafał Kowalski <rafal.kowalski@makimo.pl>
2019-07-18 16:04:25 +0200

    Fix hook on cunstruction creation

--- src/services/constructions/assign_to_owner.js
+++ src/services/constructions/assign_to_owner.js
@@ -2,11 +2,11 @@
  * Ensure that only owners of constructions
  * have access to them.
  */
-async function assignToOwner({ params: { user: owner }, app: { service } }) {
-    const users = service('users');
+async function assignToOwner({ params: { user: owner }, app, data }) {
+    const users = app.service('users');
 
     await users.patch(owner.username, {
-        constructions: [...owner.constructions, context.data.name],
+        constructions: [...owner.constructions, data.name],
     });
 }
 
