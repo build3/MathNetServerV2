1f861c2fe25354c837a320511d41c63c003f166e
Kamil Kucharski <kamil.kucharski@makimo.pl>
2019-07-22 13:36:08 +0200

    Add student permission to classes and groups services MATHNETNG-56

--- src/services/classes/classes.hooks.js
+++ src/services/classes/classes.hooks.js
@@ -1,17 +1,17 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 const checkPermissions = require('feathers-permissions');
 
-const { filterOwnedBy, setOwner, checkOwner } = require('../utils.hooks.js');
+const { filterOwnedBy, setOwner, checkOwner, isAdmin } = require('../utils.hooks.js');
 
 module.exports = {
     before: {
-        all: [authenticate('jwt'), checkPermissions({ roles: ['admin'] })],
+        all: [authenticate('jwt'), checkPermissions({ roles: ['admin', 'student'] })],
         find: [filterOwnedBy('teacher')],
         get: [filterOwnedBy('teacher')],
-        create: [setOwner('teacher')],
-        update: [checkOwner('classes', 'code', 'teacher')],
-        patch: [checkOwner('classes', 'code', 'teacher')],
-        remove: [checkOwner('classes', 'code', 'teacher')],
+        create: [isAdmin, setOwner('teacher')],
+        update: [isAdmin, checkOwner('classes', 'code', 'teacher')],
+        patch: [isAdmin, checkOwner('classes', 'code', 'teacher')],
+        remove: [isAdmin, checkOwner('classes', 'code', 'teacher')],
     },
 
     after: {
--- src/services/groups/groups.hooks.js
+++ src/services/groups/groups.hooks.js
@@ -1,18 +1,18 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 const checkPermissions = require('feathers-permissions');
 
-const { filterOwnedBy, setOwner, checkOwner } = require('../utils.hooks.js');
+const { filterOwnedBy, setOwner, checkOwner, isAdmin } = require('../utils.hooks.js');
 
 
 module.exports = {
     before: {
-        all: [authenticate('jwt'), checkPermissions({ roles: ['admin'] })],
+        all: [authenticate('jwt'), checkPermissions({ roles: ['admin', 'student'] })],
         find: [filterOwnedBy('teacher')],
         get: [filterOwnedBy('teacher')],
-        create: [setOwner('teacher')],
-        update: [checkOwner('groups', '_id', 'teacher')],
-        patch: [checkOwner('groups', '_id', 'teacher')],
-        remove: [checkOwner('groups', '_id', 'teacher')],
+        create: [isAdmin, setOwner('teacher')],
+        update: [isAdmin, checkOwner('groups', '_id', 'teacher')],
+        patch: [isAdmin, checkOwner('groups', '_id', 'teacher')],
+        remove: [isAdmin, checkOwner('groups', '_id', 'teacher')],
     },
 
     after: {
