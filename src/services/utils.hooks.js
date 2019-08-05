8e97c971e9a28a1444a4ff16f70b63a2b79941ab
Iwo Herka <iwo.herka@makimo.pl>
2019-08-05 13:48:29 +0000

    Allow to create and delete owner-less elements

--- src/services/utils.hooks.js
+++ src/services/utils.hooks.js
@@ -27,7 +27,7 @@ function checkOwner(service, queryParam, userParam) {
                 resources = resources.data;
             }
 
-            if (resources.length && resources[0][userParam] == user.username) {
+            if (resources[0][userParam] == null || (resources.length && resources[0][userParam] == user.username)) {
                 return context;
             } else {
                 throw new NotFound();
@@ -45,7 +45,7 @@ function setOwner(fieldName) {
     return function (context) {
         const { user } = context.params;
 
-        if (user !== undefined) {
+        if (user !== undefined && context.data[fieldName] === undefined) {
             context.data[fieldName] = user.username;
         }
 
