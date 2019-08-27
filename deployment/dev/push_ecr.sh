ed63d9d324c99bdf4726d384f18e8ac99166aebc
Chris Thompson <chris@233analytics.com>
2019-08-27 10:11:59 -0500

    fixed a region mismatch in the deployment scripts

--- deployment/dev/push_ecr.sh
+++ deployment/dev/push_ecr.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-$(aws ecr get-login --no-include-email --region us-east-1)
+$(aws ecr get-login --no-include-email --region us-east-2)
 docker build -t mathnet/server .
 docker tag mathnet/server:latest 617608773253.dkr.ecr.us-east-2.amazonaws.com/mathnet/server:latest
 docker push 617608773253.dkr.ecr.us-east-2.amazonaws.com/mathnet/server:latest
